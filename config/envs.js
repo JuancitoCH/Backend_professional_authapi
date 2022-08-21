fcaaab0a274a0218c172c949dfd8175c9beae7e9
Juancito <cjuan.chona@hotmail.com>
2022-08-21 20:44:15 -0300

    coneccion con db correcta y las variables de entorno tomadas correctamente

--- config/envs.js
+++ config/envs.js
@@ -1,7 +1,11 @@
 require ('dotenv').config()
 
 const config={
-    
+    port: process.env.PORT,
+    db_username: process.env.DB_USERNAME,
+    db_password: process.env.DB_PASSWORD,
+    db_host: process.env.DB_HOST,
+    db_name: process.env.DB_NAME,
 }
 
 module.exports=config
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express")
+// connectio function to db
+const { connection } = require("./config/db")
+// port of the server
+const {port} = require('./config/envs')
 const app = express()
+
+connection() //db
 
 app.get('/',(req,res)=>{
     return  res.json({
@@ -7,6 +13,6 @@ app.get('/',(req,res)=>{
     })
 })
 
-app.listen(4000,()=>{
-    console.log("Listen on http://localhost:4000")
+app.listen(port,()=>{
+    console.log(`Listen on http://localhost:${port}`)
 })
